be6ebe963edb6db5c80c07a18f12c642dd2a02b6
Wimplex <khabalexlogunov@gmail.com>
2021-08-31 15:13:48 +0300

    Bug fix

--- transformations.py
+++ transformations.py
@@ -13,7 +13,7 @@ def center(img, max_size=32):
 
 forward_transformation = T.Compose([
     T.ToTensor(),
-    T.Normalize(),
+    T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     T.Lambda(center)
 ])
 
